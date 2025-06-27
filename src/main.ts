f2f5714cb986498c8ae5fcd6ffb0b3e1b522a016
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-06-27 19:19:00 -0400

    feat: install and run ollama, add test fn

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,25 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import helmet, { contentSecurityPolicy } from 'helmet';
 import morgan from 'morgan';
+import ollama from 'ollama';
 import { appRouter } from './app.router';
 import { dataSource } from './database/data-source';
 
 dotenv.config();
+
+// TODO: Remove once no longer needed
+const testOllama = async () => {
+  // Test ollama
+  // console.log('Pulling model...');
+  // await ollama.pull({ model: 'llama3.1' });
+
+  console.log('Sending prompt...');
+  const response = await ollama.chat({
+    model: 'llama3.1',
+    messages: [{ role: 'user', content: 'Why is the sky blue?' }],
+  });
+  console.log(response.message.content);
+};
 
 (async () => {
   const app = express();
@@ -35,4 +50,6 @@ dotenv.config();
     const url = `http://localhost:${port}`;
     console.log(`Praxis CDM Service running at ${url} 🚀`);
   });
+
+  await testOllama();
 })();
