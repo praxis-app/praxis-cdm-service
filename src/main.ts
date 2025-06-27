c33d9d0ff6ee9548f9ea4325ff0429bf8437abc8
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-06-27 19:38:51 -0400

    feat: emit msg confirming ollama is ready to use

--- src/main.ts
+++ src/main.ts
@@ -4,25 +4,11 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import helmet, { contentSecurityPolicy } from 'helmet';
 import morgan from 'morgan';
-import ollama from 'ollama';
 import { appRouter } from './app.router';
 import { dataSource } from './database/data-source';
+import { testOllama } from './ollama/test-ollama';
 
 dotenv.config();
-
-// TODO: Remove once no longer needed
-const testOllama = async () => {
-  // Test ollama
-  // console.log('Pulling model...');
-  // await ollama.pull({ model: 'llama3.1' });
-
-  console.log('Sending prompt...');
-  const response = await ollama.chat({
-    model: 'llama3.1',
-    messages: [{ role: 'user', content: 'Why is the sky blue?' }],
-  });
-  console.log(response.message.content);
-};
 
 (async () => {
   const app = express();
---
+++ src/ollama/test-ollama.ts
@@ -0,0 +1,28 @@
+// TODO: Remove once no longer needed for testing
+
+import * as dotenv from 'dotenv';
+import ollama from 'ollama';
+
+dotenv.config();
+
+export const testOllama = async () => {
+  // Test ollama
+  // console.log('Pulling model...');
+  // await ollama.pull({ model: 'llama3.1' });
+
+  console.log('Sending prompt...');
+  const response = await ollama.chat({
+    model: 'llama3.1',
+    messages: [
+      {
+        role: 'user',
+        content: `
+          Emit a 10 word response declaring that Ollama is ready to use.
+          Include an emoji at the end that isn't a rocket.
+          Do not include any other text or quotes.
+        `,
+      },
+    ],
+  });
+  console.log(response.message.content);
+};
