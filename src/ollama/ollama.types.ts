303181cb1840471f647498b7a6bc8ec1884461f4
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-07-01 13:41:04 -0400

    refactor: add comments for Model type

--- src/ollama/ollama.types.ts
+++ src/ollama/ollama.types.ts
@@ -1,7 +1,13 @@
 import { ChatRequest } from 'ollama';
 
 /** Ollama models that are currently leveraged by the service. */
-export type Model = 'llama3.1' | 'llama3.2:1b' | 'llama3.2:3b' | 'gemma3:1b';
+export type Model =
+  // "State of the art" model, defaults to 8B - https://ollama.com/library/llama3.1
+  | 'llama3.1'
+  // A lightweight text only model - https://ollama.com/library/llama3.2:1b
+  | 'llama3.2:1b'
+  // A very small text only model, runs on a single GPU - https://ollama.com/library/gemma3:1b
+  | 'gemma3:1b';
 
 export interface PromptTemplate
   extends Pick<ChatRequest, 'format' | 'options'> {
