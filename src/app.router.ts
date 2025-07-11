c9fabbf9df8ad36025b2313f05d5d8fe189a0341
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-07-11 00:04:51 -0400

    refactor: remove unneeded llm routes

--- src/app.router.ts
+++ src/app.router.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { chatAnalysisRouter } from './chat-analysis/chat-analysis.router';
 import { healthRouter } from './health/health.router';
 import { matrixRouter } from './matrix/matrix.router';
 import { ollamaRouter } from './ollama/ollama.router';
@@ -10,7 +9,6 @@ appRouter.get('/', (_req, res) => {
   res.send('Welcome to the Praxis CDM Service!');
 });
 
-appRouter.use('/chat-analysis', chatAnalysisRouter);
 appRouter.use('/ollama', ollamaRouter);
 appRouter.use('/matrix', matrixRouter);
 appRouter.use('/health', healthRouter);
